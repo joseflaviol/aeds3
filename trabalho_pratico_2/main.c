87a1a2bcfd3f03374543b2499f6adf18bca12b04
José Flávio <joseflaviol119@gmail.com>
2022-07-14 19:55:24 -0300

    pseudo finalizado

---
+++ trabalho_pratico_2/main.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int **matrizAdj;
+int **predecessores;
+
+void montaMatriz(char *nomeArq, int *numeroVertices);
+void floydWarshall(int numeroVertices);
+void exibeCaminho(int inicio, int destino);
+
+int main(int argc, char **argv) {
+
+    if (argc < 2) {
+        printf("Informe o nome do arquivo\n");
+        exit(0);
+    } 
+
+    int numeroVertices;
+
+    printf("%s\n", argv[1]);
+
+    montaMatriz(argv[1], &numeroVertices);
+    floydWarshall(numeroVertices);
+
+    return 0;
+
+}
+
+void montaMatriz(char *nomeArq, int *numeroVertices) {
+    int i, j, peso;
+    FILE *arq = fopen(nomeArq, "r");
+
+    if (arq == NULL) {
+        printf("Impossivel abrir arquivo\n");
+        exit(0);
+    }
+
+    fscanf(arq, "%d", numeroVertices);
+
+    matrizAdj = (int **) malloc(sizeof(int *) * (*numeroVertices));
+
+    for (i = 0; i < *numeroVertices; i++) {
+        matrizAdj[i] = (int *) calloc(*numeroVertices, sizeof(int));
+    }
+
+    while(fscanf(arq, "%d %d %d", &i, &j, &peso) != EOF) {
+        matrizAdj[i][j] = peso;
+        matrizAdj[j][i] = peso;
+    }
+
+}
+
+void floydWarshall(int numeroVertices) {
+    int i, j, k;
+    int maiorJ, maiorK, maiorPeso;
+    int **mFloyd;
+
+    mFloyd = (int **) malloc(sizeof(int *) * numeroVertices);
+    predecessores = (int **) malloc(sizeof(int *) * numeroVertices);
+
+    for (i = 0; i < numeroVertices; i++) {
+        mFloyd[i] = (int *) malloc(sizeof(int) * numeroVertices);
+        predecessores[i] = (int *) calloc(numeroVertices, sizeof(int));
+        for (j = 0; j < numeroVertices; j++) {
+            mFloyd[i][j] = i == j ? 0 : matrizAdj[i][j] == 0 ? 10000 : matrizAdj[i][j];
+            predecessores[i][j] = i;
+        }
+    }
+
+    maiorPeso = 0;
+    for (i = 0; i < numeroVertices; i++) {
+        for (j = 0; j < numeroVertices; j++) {
+            for (k = 0; k < numeroVertices; k++) {
+                if (mFloyd[j][i] + mFloyd[i][k] < mFloyd[j][k]) {
+                    mFloyd[j][k] = mFloyd[j][i] + mFloyd[i][k];
+                    predecessores[j][k] = predecessores[i][k];
+                }
+                if (i + 1 == numeroVertices && mFloyd[j][k] > maiorPeso) {
+                    maiorJ = j;
+                    maiorK = k;
+                    maiorPeso = mFloyd[j][k];
+                } 
+            }
+        }
+    }
+
+    exibeCaminho(maiorJ, maiorK);
+    printf("\n%d\n", maiorPeso);
+
+}
+
+void exibeCaminho(int inicio, int destino) {
+    if (inicio != destino) {
+        exibeCaminho(inicio, predecessores[inicio][destino]);
+    }
+    printf("%d ", destino);
+}
